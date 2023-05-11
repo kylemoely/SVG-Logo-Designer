fada4a298c021483b9db959ad7fdab2acf5b6707
kylemoely <kylemoely@gmail.com>
2023-05-10 22:05:36 -0500

    Wrote inquirer method to ask user questions and generate logo based on answers

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const Shape = require('./lib/shapes');
+const Triangle = require('./lib/triangle');
+const Square = require('./lib/square');
+const Circle = require('./lib/circle');
 
 const checkLength = async (input) => {
     const answer = await input;
@@ -28,21 +31,32 @@ questions = [
     {
         type: 'input',
         message: 'Enter the desired color for your logo:',
-        name: 'shape-color'
+        name: 'shapeColor'
     },
     {
         type: 'input',
         message: 'Enter the desired color for the text in your logo:',
-        name: 'text-color'
+        name: 'textColor'
     }
 ]
 // Ask questions
-inquirer.prompt(questions);
-
-// Create shape corresponding to user answer
-
-// Shape constructor takes color and initials parameter
-
-// Shape has .renderFile method and .renderText method
-
-// Specific shapes have .renderShape method
+inquirer.prompt(questions)
+.then((answers) => {
+    let newLogo;
+    let color = answers.shapeColor;
+    let initials = answers.text;
+    let textColor = answers.textColor;
+    if(answers.shape==="Circle"){
+        newLogo = new Circle(color, initials, textColor);
+    } else if(answers.shape==="Square"){
+        newLogo = new Square(color, initials, textColor);
+    } else if(answers.shape==="Triangle"){
+        newLogo = new Triangle(color, initials, textColor);
+    }
+    let text = newLogo.renderText();
+    let shape = newLogo.renderShape();
+    let newFile = newLogo.renderSVG(text, shape);
+    fs.writeFile('logo.svg', newFile, (err) => {
+        err ? console.error(err) : console.log("Logo created!");
+    })
+}) 
